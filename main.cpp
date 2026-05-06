6d2190bedd83aef124937929d4381906759cb9ba
MD-YUSUF-1 <yusufsarkar2347@gmail.com>
2026-05-07 03:53:49 +0600

    Bench added

--- main.cpp
+++ main.cpp
@@ -5,8 +5,107 @@
 #define PI 3.14159265358979323846
 
 // Airplane animation
-GLfloat planeX = 1.2f;
-GLfloat planeSpeed = 0.004f;
+GLfloat planeY = 0.7f;
+GLfloat planeUpperLimit = 0.85f;
+GLfloat planeLowerLimit = 0.62f;
+
+
+GLfloat planePosition = 0.05f;
+GLfloat planeSpeed = 0.005f;
+
+GLfloat cloudPosition1 = -0.75f;
+GLfloat cloudPosition2 = -0.35f;
+GLfloat cloudPosition3 = 0.70f;
+GLfloat cloudSpeed = 0.002f;
+
+int rainMode = 0;
+float rainDropY[600];
+float rainDropX[600];
+int nightMode = 0;
+
+void drawStars()
+{
+    glPointSize(2.0f);
+
+    glBegin(GL_POINTS);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glVertex2f(-0.9f, 0.9f);
+    glVertex2f(-0.7f, 0.8f);
+    glVertex2f(-0.5f, 0.95f);
+    glVertex2f(-0.2f, 0.75f);
+    glVertex2f(0.0f, 0.92f);
+    glVertex2f(0.2f, 0.82f);
+    glVertex2f(0.5f, 0.90f);
+    glVertex2f(0.8f, 0.78f);
+    glVertex2f(0.65f, 0.96f);
+
+    glEnd();
+}
+
+void initRain() {
+    for (int i = 0; i < 600; i++) {
+
+        // X between -1 and 1
+        rainDropX[i] = ((rand() % 2000) / 1000.0f) - 1.0f;
+
+        // Y between -1 and 1
+        rainDropY[i] = ((rand() % 2000) / 1000.0f) - 1.0f;
+    }
+}
+
+
+
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 'r' || key == 'R') {
+        rainMode = !rainMode;
+    }
+    if (key == 'n' || key == 'N')
+    {
+        nightMode = !nightMode;
+    }
+}
+
+void SpecialInput(int key, int x, int y)
+{
+    switch(key)
+    {
+        case GLUT_KEY_UP:
+
+            planeY += 0.05f;
+
+            // Upper limit
+            if(planeY > planeUpperLimit)
+                planeY = planeUpperLimit;
+
+            break;
+
+        case GLUT_KEY_DOWN:
+
+            planeY -= 0.05f;
+
+            // Lower limit
+            if(planeY < planeLowerLimit)
+                planeY = planeLowerLimit;
+
+            break;
+
+        case GLUT_KEY_LEFT:
+            break;
+
+        case GLUT_KEY_RIGHT:
+            break;
+    }
+
+    glutPostRedisplay();
+}
+
+void init() {
+
+    glClearColor(0.53f, 0.81f, 0.92f, 1.0f);
+}
+
 
 
 void drawEllipse(GLfloat x, GLfloat y, GLfloat radiusX, GLfloat radiusY, int triangleAmount) {
@@ -25,9 +124,6 @@ void drawEllipse(GLfloat x, GLfloat y, GLfloat radiusX, GLfloat radiusY, int tri
     glEnd();
 }
 
-void init() {
-    glClearColor(0.53f, 0.81f, 0.92f, 1.0f);
-}
 
 void drawFilledCircle(GLfloat x, GLfloat y, GLfloat radius, int triangleAmount) {
     glBegin(GL_TRIANGLE_FAN);
@@ -60,13 +156,53 @@ void drawTriangle(GLfloat x, GLfloat y, GLfloat w, GLfloat h) {
 }
 
 
-void drawSun() {
-    glColor3f(1.0f, 1.0f, 0.0f);
-    drawFilledCircle(0.45f, 0.75f, 0.15f, 100);
+void drawSun()
+{
+    // NIGHT MODE
+    if(nightMode)
+    {
+        // Moon
+        glColor3f(0.9f, 0.9f, 0.95f);
+        drawFilledCircle(0.45f, 0.75f, 0.12f, 100);
+
+        // Moon shadow
+        glColor3f(0.05f, 0.05f, 0.12f);
+        drawFilledCircle(0.49f, 0.78f, 0.10f, 100);
+    }
+
+    // RAIN MODE
+    else if(rainMode)
+    {
+        glColor3f(0.25f, 0.25f, 0.32f);
+        drawFilledCircle(0.45f, 0.75f, 0.15f, 100);
+    }
+
+    // DAY MODE
+    else
+    {
+        glColor3f(1.0f, 1.0f, 0.0f);
+        drawFilledCircle(0.45f, 0.75f, 0.15f, 100);
+    }
 }
 
+void updateCloud(int v)
+{
+    if(cloudPosition1 > 1.9 && !rainMode)
+        cloudPosition1 = -1.9f;
+    if (cloudPosition2 >1.4 && !rainMode)
+            cloudPosition2 = -1.4;
+    if (cloudPosition3 >1.0 && !rainMode)
+            cloudPosition3 = -1.0;
 
+    cloudPosition1 = cloudPosition1 + cloudSpeed;
+    cloudPosition2 = cloudPosition2 + cloudSpeed;
+    cloudPosition3 = cloudPosition3 + cloudSpeed;
+
+	glutPostRedisplay();
+	glutTimerFunc(10, updateCloud, 0);
+}
 void drawCloud(GLfloat cx, GLfloat cy, GLfloat size) {
+    glPushMatrix();
     glColor3f(1.0f, 1.0f, 1.0f);
     drawFilledCircle(cx, cy, size, 30);
 
@@ -97,75 +233,107 @@ void drawCloud(GLfloat cx, GLfloat cy, GLfloat size) {
     glTranslatef(0.09f, 0.05f, 0.0f);
     drawFilledCircle(cx, cy, size*1.15, 30);
 
-    glLoadIdentity();
+    glPopMatrix();
 }
 
 
 void drawCityBuildings() {
-    glColor3f(0.55f, 0.60f, 0.68f);
-    drawQuad(-0.5f, 0.0f, 0.1f, 0.20f);
 
-    glTranslatef(+0.1f, 0.0f, 0.0f);
+    glPushMatrix();  // isolate everything
+
+    if(nightMode)
+    glColor3f(0.20f, 0.20f, 0.25f);
+else
+    glColor3f(0.55f, 0.60f, 0.68f);
+
+    // Base X position
+    float x = -0.5f;
+
+    // Building 1
+    drawQuad(x, 0.0f, 0.1f, 0.20f);
+
+    // Building 2
+    x += 0.1f;
     glPushMatrix();
-    glScalef(1.0,1.5,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 1.5, 1.0);
     drawQuad(-0.5f, 0.0f, 0.1f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.1f, 0.0f, 0.0f);
+    // Building 3
+    x += 0.1f;
     glPushMatrix();
-    glScalef(1.0,0.7,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 0.7, 1.0);
     drawQuad(-0.5f, 0.0f, 0.08f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.08f, 0.0f, 0.0f);
+    // Building 4
+    x += 0.08f;
     glPushMatrix();
-    glScalef(1.0,1.2,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 1.2, 1.0);
     drawQuad(-0.5f, 0.0f, 0.09f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.09f, 0.0f, 0.0f);
+    // Building 5
+    x += 0.09f;
     glPushMatrix();
-    glScalef(1.0,2.0,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 2.0, 1.0);
     drawQuad(-0.5f, 0.0f, 0.1f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.1f, 0.0f, 0.0f);
+    // Building 6
+    x += 0.1f;
     glPushMatrix();
-    glScalef(1.0,1.0,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 1.0, 1.0);
     drawQuad(-0.5f, 0.0f, 0.12f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.12f, 0.0f, 0.0f);
+    // Building 7
+    x += 0.12f;
     glPushMatrix();
-    glScalef(1.0,1.3,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 1.3, 1.0);
     drawQuad(-0.5f, 0.0f, 0.1f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.1f, 0.0f, 0.0f);
+    // Building 8
+    x += 0.1f;
     glPushMatrix();
-    glScalef(1.0,0.9,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 0.9, 1.0);
     drawQuad(-0.5f, 0.0f, 0.09f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.09f, 0.0f, 0.0f);
+    // Building 9
+    x += 0.09f;
     glPushMatrix();
-    glScalef(1.0,0.7,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 0.7, 1.0);
     drawQuad(-0.5f, 0.0f, 0.08f, 0.20f);
     glPopMatrix();
 
-    glTranslatef(+0.08f, 0.0f, 0.0f);
+    // Building 10
+    x += 0.08f;
     glPushMatrix();
-    glScalef(1.0,0.3,0);
+    glTranslatef(x + 0.5f, 0.0f, 0.0f);
+    glScalef(1.0, 0.3, 1.0);
     drawQuad(-0.5f, 0.0f, 0.08f, 0.20f);
-    glLoadIdentity();
+    glPopMatrix();
 
-
+    glPopMatrix();
 }
 
 
-// Draw grass
+
 void drawGrass() {
     // Main grass area
+   if(nightMode)
+    glColor3f(0.10f, 0.35f, 0.10f);
+else
     glColor3f(0.30f, 0.70f, 0.20f);
     glBegin(GL_QUADS);
     glVertex2f(-1.0f, -1.0f);
@@ -175,6 +343,9 @@ void drawGrass() {
     glEnd();
 
 
+    if(nightMode)
+    glColor3f(0.10f, 0.35f, 0.10f);
+else
     glColor3f(0.4f, 0.8f, 0.22f);
     glBegin(GL_TRIANGLE_FAN);
     glVertex2f(-0.4f, 0.01f);
@@ -197,7 +368,7 @@ void drawGrass() {
 
 
 void drawPineTree(GLfloat x, GLfloat y) {
-
+    glPushMatrix();
     glTranslatef(x, y, 0.0f);
     glColor3f(0.45f, 0.28f, 0.10f);
     glPushMatrix();
@@ -222,7 +393,8 @@ void drawPineTree(GLfloat x, GLfloat y) {
         drawTriangle(0.0f, 0.0f, 0.12f, 0.12f);
     glPopMatrix();
 
-    glLoadIdentity();
+    glPopMatrix();
+
 }
 
 
@@ -322,7 +494,8 @@ void drawPond() {
 
 
 void drawRoundTree(GLfloat x, GLfloat y ) {
-
+    glPushMatrix();
+    glTranslatef(x, y, 0.0f);
     glColor3f(0.45f, 0.28f, 0.10f);
     glBegin(GL_QUADS);
     glVertex2f(-0.85f, 0.027f);
@@ -331,7 +504,7 @@ void drawRoundTree(GLfloat x, GLfloat y ) {
     glVertex2f(-0.87f, -0.32f);
     glEnd();
 
-     glBegin(GL_QUADS);
+    glBegin(GL_QUADS);
     glVertex2f(-0.83f, -0.09f);
     glVertex2f(-0.93f, -0.023f);
     glVertex2f(-0.91f, -0.0f);
@@ -349,7 +522,7 @@ void drawRoundTree(GLfloat x, GLfloat y ) {
     drawFilledCircle(-0.83,0.18, 0.16, 30);
     drawFilledCircle(-0.66,0.072, 0.13, 30);
     drawFilledCircle(-1.0,0.062, 0.13, 30);
-
+    glPopMatrix();
 }
 
 
@@ -466,7 +639,10 @@ glColor3f(0.50f, 0.50f, 0.55f);
 
 void drawSwingSet() {
 
+    glPushMatrix();  // START
+
     glColor3f(0.44f, 0.27f, 0.10f);
+
     glLineWidth(3.5f);
     glBegin(GL_LINES);
     glVertex2f(0.44f,-0.008f);
@@ -475,17 +651,16 @@ void drawSwingSet() {
     glVertex2f(0.48f,-0.25f);
     glEnd();
 
-
-    glTranslatef(+0.35f, 0.0f, 0.0f);
-    glLineWidth(3.5f);
+    glPushMatrix();
+    glTranslatef(0.35f, 0.0f, 0.0f);
     glBegin(GL_LINES);
     glVertex2f(0.44f,-0.008f);
     glVertex2f(0.38f,-0.25f);
     glVertex2f(0.44f,-0.008f);
     glVertex2f(0.48f,-0.25f);
     glEnd();
+    glPopMatrix();
 
-    glTranslatef(-0.35f, 0.0f, 0.0f);
     glLineWidth(3.5f);
     glBegin(GL_LINES);
     glVertex2f(0.44f,-0.008f);
@@ -525,9 +700,20 @@ void drawSwingSet() {
     glVertex2f(0.70f,-0.008f);
     glVertex2f(0.70f,-0.18f);
     glEnd();
+    glPopMatrix();
 
 }
 
+void update(int v)
+{
+    if(planePosition > 1.0 && !rainMode && !nightMode)
+        planePosition = -1.0f;
+
+    planePosition = planePosition + planeSpeed;
+
+	glutPostRedisplay();
+	glutTimerFunc(10, update, 0);
+}
 
 void drawAirplane(float x, float y) {
 
@@ -569,18 +755,90 @@ void drawAirplane(float x, float y) {
 }
 
 
+void drawBench(float x, float y) {
+    glPushMatrix();
+    glTranslatef(x, y, 0.0f);
 
+    glColor3f(0.2f, 0.2f, 0.2f);
+    glBegin(GL_QUADS);
+    glVertex2f(0.032f, -0.02f);
+    glVertex2f(0.052f, -0.02f);
+    glVertex2f(0.052f, 0.06f);
+    glVertex2f(0.032f, 0.06f);
+
+    glVertex2f(0.15f, -0.02f);
+    glVertex2f(0.13f, -0.02f);
+    glVertex2f(0.13f,  0.06f);
+    glVertex2f(0.15f, 0.06f);
+    glEnd();
+
+
+    // Bench seat
+    glColor3f(0.55f, 0.27f, 0.07f);
+    glBegin(GL_QUADS);
+    glVertex2f(0.0f, 0.0f);
+    glVertex2f(0.18f, 0.0f);
+    glVertex2f(0.18f, -0.03f);
+    glVertex2f(0.0f, -0.03f);
+    glEnd();
+
+    // Backrest
+    glBegin(GL_QUADS);
+    glVertex2f(0.0f, 0.05f);
+    glVertex2f(0.18f, 0.05f);
+    glVertex2f(0.18f, 0.02f);
+    glVertex2f(0.0f, 0.02f);
+    glEnd();
+
+    // Legs
+    glColor3f(0.2f, 0.2f, 0.2f);
+    glBegin(GL_QUADS);
+    glVertex2f(0.01f, -0.03f);
+    glVertex2f(0.03f, -0.03f);
+    glVertex2f(0.03f, -0.08f);
+    glVertex2f(0.01f, -0.08f);
+
+    glVertex2f(0.15f, -0.03f);
+    glVertex2f(0.17f, -0.03f);
+    glVertex2f(0.17f, -0.08f);
+    glVertex2f(0.15f, -0.08f);
+    glEnd();
+    glPopMatrix();
+}
 
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
+if(nightMode)
+{
+    // Dark blue night sky
+    glClearColor(0.05f, 0.05f, 0.12f, 1.0f);
+}
+else if(rainMode)
+{
+    // Rain sky
+    glClearColor(0.25f, 0.25f, 0.32f, 1.0f);
+}
+else
+{
+    // Day sky
+    glClearColor(0.53f, 0.81f, 0.92f, 1.0f);
+}
+
+
 
     drawSun();
 
-    drawCloud(-0.75f, 0.82f, 0.055f);
-    drawCloud(-0.35f, 0.65f, 0.055f);
-    drawCloud(0.70f,0.85f, 0.055f);
+    if(nightMode)
+{
+    drawStars();
+}
+
+
+    drawCloud(cloudPosition1, 0.82f, 0.055f);
+    drawCloud(cloudPosition2, 0.65f, 0.055f);
+    drawCloud(cloudPosition3,0.85f, 0.055f);
 
     drawCityBuildings();
 
@@ -601,14 +859,51 @@ void display() {
     drawPineTree( 0.70f,0.0f);
     glPopMatrix();
 
-    drawRoundTree(-0.85f, -0.35f);
+    drawRoundTree(0.0f, 0.0f);
 
+    glPushMatrix();
     glTranslatef(0,-0.2f,0);
     drawSwingSet();
-    glLoadIdentity();
+    glPopMatrix();
+    drawAirplane(planePosition, planeY);
 
-    drawAirplane(0.05f, 0.633f);
 
+    // Benches near the path
+drawBench(-0.3f, 0.05f);
+drawBench(0.3f, 0.05f);
+
+
+    if (rainMode)
+{
+    glColor3f(0.75f, 0.75f, 1.0f);
+
+    glLineWidth(1.2f);
+
+    glBegin(GL_LINES);
+
+    for (int i = 0; i < 600; i++)
+    {
+        rainDropY[i] -= 0.03f;
+
+        // Reset rain drop
+        if (rainDropY[i] < -1.0f)
+        {
+            rainDropY[i] = 1.0f;
+
+            rainDropX[i] =
+                ((rand() % 2000) / 1000.0f) - 1.0f;
+        }
+
+        glVertex2f(rainDropX[i], rainDropY[i]);
+
+        glVertex2f(
+            rainDropX[i] + 0.015f,
+            rainDropY[i] - 0.05f
+        );
+    }
+
+    glEnd();
+}
     glFlush();
 }
 
@@ -619,7 +914,12 @@ int main(int argc, char** argv) {
     glutInitWindowPosition(80, 50);
     glutCreateWindow("Park Scenario");
     init();
+    initRain();
     glutDisplayFunc(display);
+    glutTimerFunc(100, update, 0);
+    glutTimerFunc(100, updateCloud, 0);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(SpecialInput);
     glutMainLoop();
 
 }
